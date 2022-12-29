cd96912d7f2495f4914cd003ad5d5b957ce1c3f9
Jiwoon <kfromh0136@Naver.com>
2022-12-29 14:33:37 +0900

    #4 - 메모추가, type query 작성
    `!` -> 느낌표를 붙이지 않으면 Nullable Type이 됨. 따라서, 지정해논 타입과 다른 타입을 요청하거나, 데이터가 NULL인 상태의 타입을 요청하게 되면 에러가 발생한다.

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
-
-
-
+    type User {
+        id: ID!
+        username: String!
+        firtName: String!
+        lastName: String!
+    }
+    type Tweet {
+        id: ID!
+        text: String!
+        author: User!
+    }
+    type Query {
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+    }
+    type Mutation {
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id:ID!): Boolean!
+    }
 `;
 
-const server = new ApolloServer({typeDefs})
+const server = new ApolloServer({ typeDefs })
 
-server.listen().then(({url}) => {
-    console.log(`Running on ${url}`)
+server.listen().then(({ url }) => {
+    console.log(`Running on ${url}`);
 });
 
