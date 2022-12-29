5f369318dc61429f3ccac8b6c4a11ca92cd3f010
Jiwoon <kfromh0136@Naver.com>
2022-12-29 13:21:46 +0900

    #3 - Query Type
    gql에게 query type을 설명해 주어야 하는데, type Query 구문을 필수적으로 적어주어야함.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import { ApolloServer, gql } from "apollo-server-express";
 
-const server = new ApolloServer({})
+const typeDefs = gql`
+
+
+
+`;
+
+const server = new ApolloServer({typeDefs})
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`)
 });
+
